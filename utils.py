c3500f72e2c737b2a27e6bcfcc73b85d2d36ed56
ita <itamar@trellis.ag>
2020-08-30 17:23:25 +0300

    create_new_ref_with_freqs: fix min coverage bug

--- utils.py
+++ utils.py
@@ -58,31 +58,28 @@ def get_sequence_from_fasta(fasta_file):
         records = list(SeqIO.parse(handle, "fasta"))
     if len(records) != 1:
         raise Exception("fasta file must contain only one record!")
-    return records[0].seq
+    return str(records[0].seq)
 
 
-def make_reference_from_freqs(reference_fasta_file, freqs_file, min_coverage, output_file, drop_indels):
+def create_new_ref_with_freqs(reference_fasta_file, freqs_file, min_coverage, output_file, drop_indels):
+    # TODO: what about deletions in the start or begining?
+    """Create reference from freqs filling unaligned parts with the given reference file."""
     with open(reference_fasta_file, "r") as handle:
         records = list(SeqIO.parse(handle, "fasta"))
     if len(records) != 1:
         raise Exception("fasta file must contain only one record!")
     record = records[0]
-    ref = pd.DataFrame(list(str(record.seq)), columns=['ref_base'])
-    ref.index = ref.index + 1
+    ref = pd.DataFrame(list(str(record.seq)), columns=['ref_base_from_fasta'])
+    ref.index = (ref.index + 1).astype(float)
     df = pd.read_table(freqs_file)
-    df = df[df["coverage"] >= min_coverage]
+    df.loc[df["coverage"] >= min_coverage, 'coverage'] = None
     df = df[df["base_rank"] == 0]
-    in_or_ex = 'in'
     if drop_indels:
         df = df[df["ref_pos"] == np.round(df['ref_pos'])]   # drop insertions
         df = df[df["read_base"] != "-"]                     # drop deletions
-        in_or_ex = 'ex'
-    # next line spawns a warning but it's fine.
     df = df.merge(ref, left_on='ref_pos', right_index=True, how='outer').sort_values(by='ref_pos')
-    new_seq = df['read_base'].fillna(df['ref_base']).replace('-', np.nan).dropna().str.cat()
+    new_seq = df['read_base'].fillna(df['ref_base_from_fasta']).replace('-', np.nan).dropna().str.cat()
     new_sequence = Seq.Seq(new_seq)
     record.seq = new_sequence
-    record.description = f"consensus {in_or_ex}cluding indels"
-
     with open(output_file, "w") as output_handle:
         SeqIO.write(record, output_handle, "fasta")
