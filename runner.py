e02eb2d607dadf5fabfd9c5aa6aa2872d1cb5351
ita <itamar@trellis.ag>
2020-09-09 15:00:11 +0300

    cleanups

--- runner.py
+++ runner.py
@@ -121,15 +121,13 @@ def runner(input_dir, reference_file, output_dir, stages_range, max_basecall_ite
            cpu_count):
 
     # TODO: trim read_ids to save ram
+    if not cpu_count:
+        cpu_count = mp.cpu_count()
     os.makedirs(output_dir, exist_ok=True)
     log = pipeline_logger(logger_name='AccuNGS-Runner', log_folder=output_dir)
     log.debug(f"runner params: {locals()}")
     if max_basecall_iterations is None:
         max_basecall_iterations = 2   # TODO: what should this be?!
-    log.info(cpu_count)
-    if not cpu_count:
-        cpu_count = mp.cpu_count()
-    log.info(cpu_count)
     filenames = set_filenames(output_dir)
     stages = get_stages_list(stages_range)
     log.info(f"Running stages: {stages}")
