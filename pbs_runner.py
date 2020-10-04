c3354c05e65fc08a7e3336631a14bc2e8d8324a4
ita <itamar@trellis.ag>
2020-10-04 18:43:59 +0300

    generalize defaults

--- pbs_runner.py
+++ pbs_runner.py
@@ -2,10 +2,11 @@ import os
 from random import randint
 
 from runner import create_runner_parser
+from utils import get_config
 
-
-def create_pbs_cmd_file(path, alias, output_logs_dir, cmd, queue="adistzachi@power9", gmem=2, ncpus=1, nodes=1,
-                        load_python=True, jnums=None, run_after_job_id=None):
+# TODO: fix defaults
+def create_pbs_cmd_file(path, alias, output_logs_dir, cmd, queue, gmem=10, ncpus=50, nodes=1, custom_command=None,
+                        jnums=None, run_after_job_id=None, job_suffix=None, default_command=None):
     with open(path, 'w') as o:
         o.write("#!/bin/bash\n#PBS -S /bin/bash\n#PBS -j oe\n#PBS -r y\n")
         o.write(f"#PBS -q {queue}\n")
@@ -19,12 +20,14 @@ def create_pbs_cmd_file(path, alias, output_logs_dir, cmd, queue="adistzachi@pow
                 o.write(f"#PBS -J 1-{str(jnums)} \n\n")
             else:
                 o.write(f"#PBS -J {str(jnums[0])}-{str(jnums[1])} \n\n")
-        if run_after_job_id is not None and 'adi' in queue:
-            o.write("#PBS -W depend=afterok:" + str(run_after_job_id) + ".power9.tau.ac.il\n\n")
-        if run_after_job_id is not None and 'dudu' in queue:
-            o.write("#PBS -W depend=afterok:" + str(run_after_job_id) + ".power8.tau.ac.il\n\n")
-        if load_python:
-            o.write("module load python/python-anaconda3.2019.10\n")
+        if run_after_job_id is not None:
+            if job_suffix:
+                run_after_job_id = str(run_after_job_id) + job_suffix
+            o.write("#PBS -W depend=afterok:" + str(run_after_job_id))
+        if default_command:
+            o.write(default_command)
+        if custom_command:
+            o.write(custom_command)
         o.write(cmd)
     o.close()
 
@@ -35,7 +38,7 @@ def submit_cmdfile_to_pbs(cmdfile):
     return result.split(".")[0]
 
 
-def runner_cmd(input_dir, output_dir, reference_file, stages_range, max_basecall_iterations,
+def runner_cmd(input_dir, output_dir, reference_file, stages_range, max_basecall_iterations, db_path,
                quality_threshold, task, evalue, dust, num_alignments, mode, perc_identity, soft_masking, min_coverage,
                consolidate_consensus_with_indels, stretches_pvalue, stretches_distance, stretches_to_plot,
                max_read_size, base_path, cleanup, cpu_count, overlap_notation):
@@ -79,87 +82,18 @@ def runner_cmd(input_dir, output_dir, reference_file, stages_range, max_basecall
         cmd += f" -cc {cpu_count}"
     if overlap_notation:
         cmd += f" -on {overlap_notation}"
+    if db_path:
+        cmd += f" -db {db_path}"
     return cmd
 
 
-def pbs_runner_experimental(input_dir, output_dir, reference_file, stages_range, max_basecall_iterations, part_size,
-               quality_threshold, task, evalue, dust, num_alignments, mode, perc_identity, soft_masking, min_coverage,
-               consolidate_consensus_with_indels, stretches_pvalue, stretches_distance, stretches_to_plot,
-               max_read_size, alias, queue, num_of_nucs):
-    # TODO: move defaults to a config file
-    serial_job_id = None
-    haplo_job_id = None
-    graph_job_id = None
-    jobs_submitted = []
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    pbs_logs_dir = os.path.join(output_dir, "pbs_logs")
-    os.makedirs(pbs_logs_dir, exist_ok=True)
-    if isinstance(stages_range, int):
-        stages_range = [stages_range, stages_range]
-    if min(stages_range) <= 3:
-        serial_cmdfile = os.path.join(pbs_logs_dir, f"AccuNGS_123.cmd")
-        stages_123 = (stages_range[0], min(3, stages_range[1]))
-        cmd = runner_cmd(input_dir=input_dir, output_dir=output_dir, reference_file=reference_file,
-                         stages_range=stages_123, max_basecall_iterations=max_basecall_iterations,
-                         part_size=part_size, quality_threshold=quality_threshold, task=task, evalue=evalue, dust=dust,
-                         num_alignments=num_alignments, mode=mode, perc_identity=perc_identity,
-                         soft_masking=soft_masking, min_coverage=min_coverage,
-                         consolidate_consensus_with_indels=consolidate_consensus_with_indels,
-                         stretches_pvalue=stretches_pvalue, stretches_distance=stretches_distance,
-                         stretches_to_plot=stretches_to_plot, max_read_size=max_read_size, base_path=base_path)
-        alias = "AccuNGS_123"
-        create_pbs_cmd_file(serial_cmdfile, alias, output_logs_dir=pbs_logs_dir, cmd=cmd, queue=queue, gmem=100,
-                            ncpus=40)
-        serial_job_id = submit_cmdfile_to_pbs(serial_cmdfile)
-        jobs_submitted.append(serial_job_id)
-    if (min(stages_range) <= 4) and (max(stages_range) >= 4):
-        freqs_file_path = os.path.join(output_dir, 'freqs.tsv')
-        alias = "AccuNGS_haplo"
-        compute_haplo_path = os.path.join(pbs_logs_dir, f"AccuNGS_4.cmd")
-        linked_mutations_dir = os.path.join(output_dir, 'processing', 'linked_mutations')
-        mutations_linking_path = os.path.join(base_path, 'mutations_linking.py')
-        cmd = f"python {mutations_linking_path} -x $PBS_ARRAY_INDEX -f {freqs_file_path} " \
-              f"-r {output_dir}/mutation_read_list.tsv " \
-              f"-o {linked_mutations_dir}/$PBS_ARRAY_INDEX_linked_mutations.tsv"
-        if max_read_size:
-            cmd += f" -m {max_read_size}"
-        haplo_logs = os.path.join(pbs_logs_dir, 'linked_mutations_logs')
-        os.makedirs(haplo_logs, exist_ok=True)
-        create_pbs_cmd_file(compute_haplo_path, alias, output_logs_dir=haplo_logs, cmd=cmd, queue=queue, gmem=20,
-                            ncpus=1, jnums=num_of_nucs, run_after_job_id=serial_job_id)
-        haplo_job_id = submit_cmdfile_to_pbs(compute_haplo_path)
-        jobs_submitted.append(haplo_job_id)
-    if 5 in stages_range:
-        alias = "AccuNGS_graph"
-        graph_haplo_path = os.path.join(pbs_logs_dir, "AccuNGS_5.cmd")
-        cmd = runner_cmd(input_dir=input_dir, output_dir=output_dir, reference_file=reference_file,
-                         stages_range=5, max_basecall_iterations=max_basecall_iterations,
-                         part_size=part_size, quality_threshold=quality_threshold, task=task, evalue=evalue, dust=dust,
-                         num_alignments=num_alignments, mode=mode, perc_identity=perc_identity,
-                         soft_masking=soft_masking, min_coverage=min_coverage,
-                         consolidate_consensus_with_indels=consolidate_consensus_with_indels,
-                         stretches_pvalue=stretches_pvalue, stretches_distance=stretches_distance,
-                         stretches_to_plot=stretches_to_plot, max_read_size=max_read_size, base_path=base_path)
-        create_pbs_cmd_file(graph_haplo_path, alias, output_logs_dir=pbs_logs_dir, cmd=cmd, queue=queue, gmem=2,
-                            ncpus=1, run_after_job_id=haplo_job_id)
-        graph_job_id = submit_cmdfile_to_pbs(graph_haplo_path)
-        jobs_submitted.append(graph_job_id)
-    print(f"Jobs submitted: {jobs_submitted}")
-    print(f"Output files will be in {output_dir} ")
-    print(f"cmd files and pbs logs under will be in {pbs_logs_dir} ")
-    print(f"runner log file will be in {output_dir}.log ")
-
-
-def pbs_runner(input_dir, output_dir, reference_file, stages_range, max_basecall_iterations,
+def pbs_runner(input_dir, output_dir, reference_file, stages_range, max_basecall_iterations,db_path,
                quality_threshold, task, evalue, dust, num_alignments, mode, perc_identity, overlap_notation,
-               after_jobid=None, soft_masking="F",
-               min_coverage=10, consolidate_consensus_with_indels=True, stretches_pvalue=1e-7, stretches_distance=10,
-               stretches_to_plot=5,
-               max_read_size=350, alias="AccuNGS", queue="adistzachi@power9", cleanup=True, cpu_count=None):
-    # TODO: move defaults to a config file
-    # TODO: optimize part size depending on input size and number of CPUs
-    if not cpu_count:
-        cpu_count = 50  # todo: optimize this. a high number could get jobs queued. with 100 I could only run 2 jobs.
+               soft_masking, min_coverage, consolidate_consensus_with_indels, stretches_pvalue, stretches_distance,
+               stretches_to_plot, max_read_size, alias, queue, cleanup, cpu_count, custom_command=None, after_jobid=None,
+               job_suffix=None):
+    config = get_config()
+    defaults = config['pbs_defaults']
     base_path = os.path.dirname(os.path.abspath(__file__))
     pbs_logs_dir = os.path.join(output_dir, "pbs_logs")
     os.makedirs(pbs_logs_dir, exist_ok=True)
@@ -173,12 +107,14 @@ def pbs_runner(input_dir, output_dir, reference_file, stages_range, max_basecall
                      quality_threshold=quality_threshold, task=task, evalue=evalue, dust=dust,
                      num_alignments=num_alignments, mode=mode, perc_identity=perc_identity,
                      soft_masking=soft_masking, min_coverage=min_coverage, cleanup=cleanup, cpu_count=cpu_count,
-                     consolidate_consensus_with_indels=consolidate_consensus_with_indels,
+                     consolidate_consensus_with_indels=consolidate_consensus_with_indels, db_path=db_path,
                      stretches_pvalue=stretches_pvalue, stretches_distance=stretches_distance,
                      stretches_to_plot=stretches_to_plot, max_read_size=max_read_size, base_path=base_path,
                      overlap_notation=overlap_notation)
+    default_command = defaults.get('default_command', fallback=None)
     create_pbs_cmd_file(cmd_path, alias, output_logs_dir=pbs_logs_dir, cmd=cmd, queue=queue, gmem=100,
-                        ncpus=cpu_count, run_after_job_id=after_jobid)  # todo: optimize cpu_count
+                        ncpus=cpu_count, run_after_job_id=after_jobid, job_suffix=job_suffix, custom_command=custom_command,
+                        default_command=default_command)  # todo: optimize cpu_count
     job_id = submit_cmdfile_to_pbs(cmd_path)
     print(f"Submitted jod '{alias}' with id {job_id}")
     print(f"Output files will be in {output_dir}")
@@ -189,18 +125,23 @@ def pbs_runner(input_dir, output_dir, reference_file, stages_range, max_basecall
 
 if __name__ == "__main__":
     parser = create_runner_parser()
+    # TODO: pbs-runner defaults, run with config file
     parser.add_argument("-a", "--alias", default="AccuNGS", help="job alias visible in qstat (default: AccuNGS)")
     parser.add_argument("-q", "--queue", default="adistzachi@power9",
                         help="PBS queue to run on (default: adistzachi@power9)")
     parser.add_argument("-j", "--after_jobid",
                         help="Run after successfully completing this jobid")
-    args = parser.parse_args()
-    pbs_runner(input_dir=args.input_dir, output_dir=args.output_dir, reference_file=args.reference_file,
-               stages_range=args.stages_range, max_basecall_iterations=args.max_basecall_iterations,
-               quality_threshold=args.quality_threshold, task=args.blast_task, overlap_notation=args.overlap_notation,
-               evalue=args.blast_evalue, dust=args.blast_dust, num_alignments=args.blast_num_alignments,
-               mode=args.blast_mode, perc_identity=args.blast_perc_identity, soft_masking=args.blast_soft_masking,
-               min_coverage=args.min_coverage, consolidate_consensus_with_indels=args.consolidate_consensus_with_indels,
-               stretches_pvalue=args.stretches_pvalue, stretches_distance=args.stretches_distance, alias=args.alias,
-               stretches_to_plot=args.stretches_to_plot, max_read_size=args.stretches_max_read_size, queue=args.queue,
-               cleanup=args.cleanup, cpu_count=args.cpu_count, after_jobid=args.after_jobid)
+    parser_args = vars(parser.parse_args())
+    args = dict(get_config()['pbs_defaults'])
+    args.update({key: value for key, value in parser_args.items() if value is not None})
+    pbs_runner(input_dir=args['input_dir'], output_dir=args['output_dir'], reference_file=args['reference_file'],
+               stages_range=args['stages_range'], max_basecall_iterations=args['max_basecall_iterations'],
+               quality_threshold=args['quality_threshold'], task=args['blast_task'],
+               evalue=args['blast_evalue'], dust=args['blast_dust'], num_alignments=args['blast_num_alignments'],
+               mode=args['blast_mode'], perc_identity=args['blast_perc_identity'], soft_masking=args['blast_soft_masking'],
+               min_coverage=args['min_coverage'], consolidate_consensus_with_indels=args['consolidate_consensus_with_indels'],
+               stretches_pvalue=args['stretches_pvalue'], stretches_distance=args['stretches_distance'], cleanup=args['cleanup'],
+               stretches_to_plot=args['stretches_to_plot'], max_read_size=args['stretches_max_read_size'],
+               cpu_count=args['cpu_count'], overlap_notation=args['overlap_notation'], db_path=args['db_path'],
+               after_jobid=args['after_jobid'], job_suffix=args['job_suffix'], alias=args['alias'],
+               custom_command=args['custom_command'], queue=args['queue'])
