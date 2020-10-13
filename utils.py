a2dd62ffa0673c1761a84f549e3d2d330cace4b3
ita <itamar@trellis.ag>
2020-10-13 17:16:41 +0300

    create_new_ref_with_freqs: drop low frequency insertions

--- utils.py
+++ utils.py
@@ -120,6 +120,7 @@ def create_new_ref_with_freqs(reference_fasta_file, freqs_file, min_coverage, ou
     df = pd.read_table(freqs_file)
     df = df[df["base_rank"] == 0]
     df.loc[df["coverage"] <= min_coverage, 'read_base'] = None
+    df = df[df['frequency'] > 0.5]                          # drop low frequency insertions
     if drop_indels:
         df = df[df["ref_pos"] == np.round(df['ref_pos'])]   # drop insertions
         df = df[df["read_base"] != "-"]                     # drop deletions
