96b9bf2f1f190041e3b9146d7cb5a6942198d03d
ita <itamar@trellis.ag>
2021-08-10 16:03:47 +0300

    tester wip

--- tester_pbs.py
+++ tester_pbs.py
@@ -16,6 +16,12 @@ def read_json_file(filepath):
     return data
 
 
+def wait_till_timeout(start_time, timeout):
+    if time.time() - start_time > timeout:
+        raise TimeoutError('took too long for tests to run!! check it yourself, loser11!!!')
+    sleep(5)
+
+
 def tester():
     timeout = 120
     params_file = '/sternadi/home/volume2/ita/sternlab-public/tester/params.json'
@@ -27,15 +33,14 @@ def tester():
         param_dict['output_dir'] = os.path.join(output_dir, param_dict['output_dir'])
     multi_runner(params_list)
     print('Giving the jobs a bit of time to start...')
-    sleep(20)
     meta_data_files = [os.path.join(param_dict['output_dir'], 'meta_data.json') for param_dict in params_list]
     start_time = time.time()
+    while not os.path.isfile(meta_data_files[0]):
+        wait_till_timeout(start_time, timeout)
     for meta_data_file in meta_data_files:
         meta_data = read_json_file(meta_data_file)
         while '...' in meta_data['status']:
-            if time.time()-start_time > timeout:
-                raise TimeoutError('took too long for tests to run!! check it yourself, loser11!!!')
-            sleep(5)
+            wait_till_timeout(start_time, timeout)
         status = meta_data['status'][:6]
         job_name = os.path.dirname(meta_data['output_dir'])
         if 'Done' in status:
