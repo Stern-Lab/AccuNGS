7506f9283a618866cc1253f8144c1d51dc9cfe4f
ita <itamar@trellis.ag>
2021-08-10 15:54:42 +0300

    tester wip

--- tester_pbs.py
+++ tester_pbs.py
@@ -1,4 +1,5 @@
 import os
+import time
 from datetime import datetime
 import json
 from time import sleep
@@ -8,20 +9,37 @@ from pbs_multi_runner import multi_runner
 # TODO: this should not be part of the final repo
 
 
-def tester():
-    params_file = '/sternadi/home/volume2/ita/sternlab-public/tester/params.json'
-    with open(params_file) as jsonfile:
+def read_json_file(filepath):
+    with open(filepath) as jsonfile:
         json_string = jsonfile.read()
-        params_list = json.loads(json_string)
+        data = json.loads(json_string)
+    return data
+
+
+def tester():
+    timeout = 120
+    params_file = '/sternadi/home/volume2/ita/sternlab-public/tester/params.json'
+    params_list = read_json_file(params_file)
     now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     output_dir = '/sternadi/home/volume2/ita/sternlab-public/tester/tests/' + now
     os.makedirs(output_dir, exist_ok=True)
     for param_dict in params_list:
         param_dict['output_dir'] = os.path.join(output_dir, param_dict['output_dir'])
     multi_runner(params_list)
-    print('Waiting for jobs to finish...')
-    sleep(10)
-    print(f"found {len(os.listdir(output_dir))} files!")
+    meta_data_files = [os.path.join(param_dict['output_dir'], 'meta_data.json') for param_dict in params_list]
+    start_time = time.time()
+    for meta_data_file in meta_data_files:
+        meta_data = read_json_file(meta_data_file)
+        while '...' in meta_data['status']:
+            if time.time()-start_time > timeout:
+                raise TimeoutError('took too long for tests to run!! check it yourself, loser11!!!')
+            sleep(5)
+        status = meta_data['status'][:6]
+        job_name = os.path.dirname(meta_data['output_dir'])
+        if 'Done' in status:
+            print(f"job {job_name} Done!")
+        else:
+            print(f'error in job {job_name} !')
 
 
 if __name__ == "__main__":
